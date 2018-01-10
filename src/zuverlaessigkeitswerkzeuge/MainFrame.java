03097d534198beeae12b37870a24838496a93567
VeritasCurat <johannesgrobelski@live.de>
2018-01-10 14:26:51 +0100

    .

--- src/zuverlaessigkeitswerkzeuge/MainFrame.java
+++ src/zuverlaessigkeitswerkzeuge/MainFrame.java
@@ -1,14 +1,37 @@
 package zuverlaessigkeitswerkzeuge;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 import com.sun.javafx.geom.ConcentricShapePair;
 
+class JCanvas extends JComponent
+{
+	
+   int x; int y; int height; int width;
+   
+   public void paintComponent(Graphics g) 
+   {
+    Graphics2D g2 = (Graphics2D) g;
+      super.paintComponent(g);
+      g2.setColor(Color.blue);   
+      g2.fillRect(x,y,height,width);
+   }
+ }
+
 public class MainFrame extends JFrame{
 
+	JCanvas jc = new JCanvas();
+	static MainFrame frame=new MainFrame();
+
+	
 	/**
 	 * 
 	 */
@@ -16,36 +39,24 @@ public class MainFrame extends JFrame{
 
 	public static void main(String[] args) {
 		 // TODO code application logic here
-		MainFrame MF = new MainFrame();
-		MF.init_frame();
-		MF.initComponents();
+		frame.init_frame();
+		frame.moveTo(500,0);
 	}
 	
 	private  void init_frame() {
-		 MainFrame frame=new MainFrame();
 		    frame.setTitle("Zuverlässigkeitswerkzeuge");
-		    frame.setSize(1000, 620);
+		    frame.setSize(1000, 1000);
 		    frame.setResizable(false);
 		    frame.setLocation(50, 50);
-		    frame.setVisible(true);		 
+		    frame.setVisible(true);		
+		    jc.x = 0; jc.y = 500; jc.height = 100; jc.width = 100;
+		    frame.getContentPane().add(jc);
 	}
-	
-	private void initComponents() {
-		JButton jButton1 = new javax.swing.JButton();
-		JButton jButton2 = new javax.swing.JButton();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new java.awt.FlowLayout());
- 
-        jButton1.setText("jButton1");
-        getContentPane().add(jButton1);
- 
-        jButton1.setText("jButton2");
-        getContentPane().add(jButton1);
- 
-     
- 
-        pack();
-    }
+		
+	private void moveTo(int x, int y) {
+		frame.getContentPane().remove(jc);
+		jc.x =x; jc.y = y;
+		frame.getContentPane().add(jc);
+	}
 
 }
