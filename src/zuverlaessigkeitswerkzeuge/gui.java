3b64e8848b0e58985ae0d2fd8482678fbc7d33e1
VeritasCurat <johannesgrobelski@live.de>
2018-01-10 13:30:28 +0100

    .

---
+++ src/zuverlaessigkeitswerkzeuge/gui.java
@@ -0,0 +1,22 @@
+package zuverlaessigkeitswerkzeuge;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class gui extends Composite {
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public gui(Composite parent, int style) {
+		super(parent, style);
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+
+}
