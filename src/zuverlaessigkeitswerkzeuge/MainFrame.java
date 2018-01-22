a7183ccf91b9ceef6044264fddb3659f5c02d66f
VeritasCurat <johannesgrobelski@live.de>
2018-01-22 15:23:51 +0100

    reperatur

--- src/zuverlaessigkeitswerkzeuge/MainFrame.java
+++ src/zuverlaessigkeitswerkzeuge/MainFrame.java
@@ -6,11 +6,8 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
-<<<<<<< HEAD
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-=======
->>>>>>> ce565b43bee7a2386a082da7fe72229c697a8747
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -83,38 +80,25 @@ class JCanvas extends JComponent
    
    
  }
-<<<<<<< HEAD
-public class MainFrame extends JFrame implements KeyListener,ActionListener{
-	int posX; int posY;
-	//KL Teil
-		JTextArea displayArea;
-	    JTextField typingArea;
-	
-=======
-
 public class MainFrame extends JFrame{
+
+
 	int posX; int posY; char keyTyped;
->>>>>>> ce565b43bee7a2386a082da7fe72229c697a8747
+
 	JCanvas jc = new JCanvas();
 	static MainFrame frame=new MainFrame();
 	
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-		 // TODO code application logic here
-<<<<<<< HEAD
 		
-		
-		frame.init_frame();
 
-		System.out.println("test");		
-=======
-
+	    
+	    
 		frame.init_frame();
 
 		System.out.println("test");
 		
->>>>>>> ce565b43bee7a2386a082da7fe72229c697a8747
 		frame.test(); 
 		frame.zeichneObjekte(frame.jc);
 		
@@ -129,11 +113,8 @@ public class MainFrame extends JFrame{
 		                //System.out.println("a:"+posX+" "+posY);
 		            }
 		     });
-<<<<<<< HEAD
-		        
-=======
+
 			 addKeyListener(new KeyListener() {
-				
 				@Override
 				public void keyTyped(KeyEvent e) {
 					// TODO Auto-generated method stub
@@ -152,21 +133,17 @@ public class MainFrame extends JFrame{
 					
 				}
 			});
->>>>>>> ce565b43bee7a2386a082da7fe72229c697a8747
-		
 			
 			Dimension aufloesung= Toolkit.getDefaultToolkit().getScreenSize();
 
-			//KL
-		        typingArea.addKeyListener(this);
-		        getContentPane().add(frame);
+			System.out.println(aufloesung.width+" "+aufloesung.height);
 			
-		    frame.setTitle("Zuverlässigkeitswerkzeuge");
-		    frame.setSize(aufloesung.width, aufloesung.height);
-		    frame.setResizable(false);
+
+			frame.setSize(1000,1000);
+			frame.setTitle("Zuverlässigkeitswerkzeuge");
+		    frame.setResizable(true);
 		    frame.setLocation(0, 0);
-		    frame.setVisible(true);	 frame.pack();
-		    	
+		    frame.setVisible(true);	 frame.pack();		    	
 	}
 	
 	private void test() {
@@ -188,37 +165,7 @@ public class MainFrame extends JFrame{
 		frame.repaint();
 	}
 
-	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		System.out.println("d");
 
-	}
-
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		System.out.println("b");
-	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		System.out.println("a");
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		 //Clear the text components.
-        displayArea.setText("");
-        typingArea.setText("");
-        
-        //Return the focus to the typing area.
-        typingArea.requestFocusInWindow();		
-	}
 	
-	
-		
-
 
 }
